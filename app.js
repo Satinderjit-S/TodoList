1e48b63747e98c787f50ed4cec71711d5b32cdbf
unknown <saisatinderjitsingh@gmail.com>
2019-08-12 16:10:51 -0400

    Add to list functionality added, Server running and tested , EJS added ,

---
+++ app.js
@@ -0,0 +1,33 @@
+//jshint esversion:6
+
+const express = require("express");
+const app = express();
+const bodyParser=require("body-parser");
+app.use(bodyParser.urlencoded({extended:true}));
+app.set("view engine","ejs");
+
+
+var date = new Date();
+var options= {
+  weekday:"long",
+  day:"numeric",
+  month:"long"
+};
+
+var pagedate=date.toLocaleDateString("en-us",options);
+
+var hometodo=["Cook","Clean"];
+
+app.listen(3000,function(req,res){
+  console.log("Server Running");
+});
+
+app.get('/',function(req,res){
+  res.render("list",{date:pagedate,htodo:hometodo});
+});
+
+app.post("/",function(req,res){
+  var addeditem=req.body.newitem;
+  hometodo.push(addeditem);
+  res.redirect("/");
+});
