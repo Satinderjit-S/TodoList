2af589315834d86ca9695c320e93e4a8e4224e09
unknown <saisatinderjitsingh@gmail.com>
2019-08-12 19:11:52 -0400

    Added EJS layouts, Added another route to /Work using same ejs file

--- app.js
+++ app.js
@@ -16,19 +16,38 @@ var options= {
 };
 
 var pagedate=date.toLocaleDateString("en-us",options);
-
 var hometodo=["Cook","Clean"];
+var worklistheading="Work List Items";
+var worktodo=[];
 
 app.listen(3000,function(req,res){
   console.log("Server Running");
 });
 
 app.get('/',function(req,res){
-  res.render("list",{date:pagedate,htodo:hometodo});
+  res.render("list",{listheading:pagedate,todoitems:hometodo});
+});
+
+app.post("/",function(req,res){
+  console.log(req.body);
+  var addeditem=req.body.newitem;
+  if(req.body.list === "Work" )
+  {
+      worktodo.push(addeditem);
+      res.redirect("/work");
+  }
+  else
+  {  hometodo.push(addeditem);
+    res.redirect("/");
+  }
+});
+
+app.get('/work',function(req,res){
+  res.render("list",{listheading:worklistheading,todoitems:worktodo});
 });
 
 app.post("/",function(req,res){
   var addeditem=req.body.newitem;
-  hometodo.push(addeditem);
-  res.redirect("/");
+  worktodo.push(addeditem);
+  res.redirect("/work");
 });
