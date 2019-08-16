39a8aecb4f58090a79e7cca91b7320395f5df262
unknown <saisatinderjitsingh@gmail.com>
2019-08-16 17:30:10 -0400

    Added mongoDB, implemented dynamic routing

--- app.js
+++ app.js
@@ -3,47 +3,140 @@
 const express = require("express");
 const app = express();
 const bodyParser=require("body-parser");
+const mongoose=require("mongoose");
 const day=require(__dirname+"/date.js");
+
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 app.set("view engine","ejs");
 
 
+mongoose.connect("mongodb://localhost:27017/todolistDB",{ useNewUrlParser : true });
 
-var hometodo=["Cook","Clean"];
-var worklistheading="Work List Items";
-var worktodo=[];
+//Schema creation for default list
+const itemSchema=new mongoose.Schema({
+  name:String
+});
 
+// Schema for custom list
+const listSchema=new mongoose.Schema({
+  name:String,
+  items:[itemSchema]
+});
+
+//Mongoose model
+const Item=mongoose.model("Item",itemSchema);
+const List=mongoose.model("List",listSchema);
+
+
+const item1=new Item({
+  name:"Cook"
+});
+const item2=new Item({
+  name:"Clean"
+});
+
+const defaultItems=[item1,item2];
+const defaultForCustom=[];
+var pagedate=day.getDate();
+
+//var pagedate=day.getDay(); //will give Day on the top without date and month
 app.listen(3000,function(req,res){
   console.log("Server Running");
 });
 
 app.get('/',function(req,res){
-  var pagedate=day.getDate();
-  // var pagedate=day.getDay(); //will give Day on the top without date and month
-  res.render("list",{listheading:pagedate,todoitems:hometodo});
+  Item.find(function(err,item){
+    if(item.length===0) //if the list is emply default values are populated
+    {
+      Item.insertMany(defaultItems,function(err){
+
+        if(err){
+          console.log("Error occured");
+        }
+        else{
+          console.log("Default list populated");
+        }
+
+        res.redirect('/');
+      });
+    }
+
+    else{
+    res.render("list",{listheading:pagedate,todoitems:item});
+    }
+
+  });
 });
 
 app.post("/",function(req,res){
-  console.log(req.body);
   var addeditem=req.body.newitem;
-  if(req.body.list === "Work" )
-  {
-      worktodo.push(addeditem);
-      res.redirect("/work");
-  }
-  else
-  {  hometodo.push(addeditem);
-    res.redirect("/");
-  }
+  const listName=req.body.list;
+  const newItem= new Item({
+    name:addeditem
+  });
+  console.log(pagedate);
+  if(listName===(pagedate.substr(0,pagedate.indexOf(' '))))
+    {
+    newItem.save();
+      res.redirect("/");
+    }
+    else{
+      List.findOne({name:listName},function(err,foundList)
+      {
+        foundList.items.push(newItem);
+        foundList.save();
+        res.redirect("/"+listName);
+        });
+    }
 });
 
-app.get('/work',function(req,res){
-  res.render("list",{listheading:worklistheading,todoitems:worktodo});
+
+
+app.post("/delete",function(req,res){
+  const checkeitemID=req.body.checkbox;
+  const listName=req.body.hiddenListName;
+
+  if(listName===(pagedate.substr(0,pagedate.indexOf(' '))))
+    {
+      Item.findByIdAndRemove(checkeitemID,function(err){
+        if(err){
+          console.log("Error occured while removing element");
+        }else{
+          res.redirect('/');
+          console.log("Successfully deleted item");
+        }
+        });
+    }
+    else{
+      List.findOneAndUpdate({name:listName},{$pull:{items:{_id:checkeitemID}}},function(err,foundList){
+        if(!err)
+        {
+          res.redirect("/"+listName);
+        }
+      });
+
+    }
 });
 
-app.post("/",function(req,res){
-  var addeditem=req.body.newitem;
-  worktodo.push(addeditem);
-  res.redirect("/work");
+// route for custom list
+app.get('/:listName',function(req,res){
+  const customListName=req.params.listName;
+  List.findOne({name:customListName},function(err,foundList){
+    if(!foundList)
+    {
+      const list=new List({
+        name:customListName,
+        items:defaultForCustom
+      });
+      list.save();
+      res.redirect("/"+customListName);
+    }else{
+      res.render("list",{listheading:customListName,todoitems:foundList.items});
+    }
+  });
+
+
+
+
 });
